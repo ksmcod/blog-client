e3b28a772213219bab2502ae5a2a634e320ffa0a
Kono Steve <stevekono77@gmail.com>
2023-08-06 00:05:55 +0100

    Adjusted styling for the 'Blog' element

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -4,13 +4,16 @@ export default function Blog({ img }) {
   return (
     <div className='grid grid-cols-blog sm:grid-rows-none border-b py-3'>
         <div className="">
-            <img className='w-full sm:w-auto sm:h-full' src={img} alt="" srcSet="" />
+            <img className='w-full sm:w-auto' src={img} alt="" srcSet="" />
         </div>
         <div className="px-3">
-            <h5 className="font-bold sm:max-w-md">The best apps are taking a page from mobile gaming</h5>
-            <p className="hidden sm:block">Retaining mobile app subscribers is harder than it was last year, but paywall optimization and gamified UX can help Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, quasi.</p>
+            <h5 className="font-bold sm:max-w-md leading-none">The best apps are taking a page from mobile gaming</h5>
+            <p className="hidden sm:block">Retaining mobile app subscribers is harder than it was last year, but paywall optimization and gamified UX can help.</p>
 
-            <p className="text-sm py-1">Kono Steve 2023-08-05</p>
+            <div className="mt-3 space-y-1">
+                <p className="text-sm leading-none text-slate-700 font-bold">Kono Steve</p>
+                <p className="text-sm leading-none text-gray-400"> 2023-08-05 15:34:12</p>
+            </div>
         </div>
     </div>
   )
