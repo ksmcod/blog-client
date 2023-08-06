e01b8dd360a257dd21782d6f1f6d27da19256456
Kono Steve <stevekono77@gmail.com>
2023-08-06 01:17:33 +0100

    Styled Login form

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -2,7 +2,7 @@ import { NavLink, Outlet } from "react-router-dom"
 import Navbar from "../components/Navbar"
 export default function RootLayout() {
   return (
-    <main className="border space-y-10">
+    <main className="space-y-10">
         <Navbar/>
         <Outlet/>
     </main>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,10 +2,10 @@
 
 export default function Login() {
   return (
-    <form action="">
-        <input type="email"name="email" placeholder="Enter your e-mail" />
-        <input type="text" name="password" placeholder="Enter your password" />
-        <button>Login</button>
+    <form className="pt-20 px-8 sm:px-32 max-w-3xl mx-auto flex flex-col gap-2">
+        <input className="py-1 px-2 rounded-lg focus:outline-none  border border-gray-400" type="email"name="email" placeholder="Enter your e-mail" />
+        <input className="py-1 px-2 rounded-lg focus:outline-none  border border-gray-400" type="password" name="password" placeholder="Enter your password" />
+        <button className="bg-gray-400 py-2 text-white font-bold rounded">Login</button>
     </form>
   )
 }
