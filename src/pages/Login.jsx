61f092ed655aaa05757d2b1de4acb60ca4548df9
Kono Steve <stevekono77@gmail.com>
2023-08-06 02:14:59 +0100

    Added link to signup page on login page & vice versa

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,17 @@
+import { Link } from "react-router-dom";
 
 
 export default function Login() {
   return (
-    <div className="">
-        <h2 className="text-center text-secondary">Login</h2>
+    <div className="max-w-3xl mx-auto">
+        <div className="space-y-3">
+            <h2 className="text-center text-secondary">Login to your account</h2>
+            <p className="font-bold text-center -translate-x-28">
+                Don't have an account ? <span className="text-secondary"><Link to='/register'>sign up</Link></span>
+            </p>
+        </div>
 
-        <form className="pt-10 px-8 sm:px-32 max-w-3xl mx-auto flex flex-col gap-2">
+        <form className="pt-5 px-8 sm:px-32 max-w-3xl mx-auto flex flex-col gap-2">
             <input className="py-1 px-2 rounded-lg focus:outline-secondary focus:border-secondary  border border-gray-400" type="email"name="email" placeholder="Enter your e-mail" />
 
             <input className="py-1 px-2 rounded-lg focus:outline-secondary focus:border-secondary  border border-gray-400" type="password" name="password" placeholder="Enter your password" />
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 export default function Register() {
   return (
     <div className="">
-        <h2 className="text-center text-secondary m-0">Join the community</h2>
+        <div className="space-y-3">
+            <h2 className="text-center text-secondary">Join the community</h2>
+            <p className="font-bold text-center -translate-x-24">
+                Already have an account ? <span className="text-secondary"><Link to='/login'>login</Link></span>
+            </p>
+        </div>
 
-        <form className="pt-10 px-8 sm:px-32 max-w-3xl mx-auto flex flex-col gap-2">
+        <form className="pt-5 px-8 sm:px-32 max-w-3xl mx-auto flex flex-col gap-2">
             <input className="py-1 px-2 rounded-lg focus:outline-secondary focus:border-secondary  border border-gray-400" type="text"name="username" placeholder="Enter a username" />
 
             <input className="py-1 px-2 rounded-lg focus:outline-secondary focus:border-secodary  border border-gray-400" type="email"name="email" placeholder="Enter your e-mail" />
