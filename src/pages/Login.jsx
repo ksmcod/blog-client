2b8633d5ae1b7e1e83dbfe24c92c1507d7fc7d27
Kono Steve <stevekono77@gmail.com>
2023-08-06 01:28:06 +0100

    Styled Register form, modified and styled 'Register' and 'Login' pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,10 +2,14 @@
 
 export default function Login() {
   return (
-    <form className="pt-20 px-8 sm:px-32 max-w-3xl mx-auto flex flex-col gap-2">
-        <input className="py-1 px-2 rounded-lg focus:outline-none  border border-gray-400" type="email"name="email" placeholder="Enter your e-mail" />
-        <input className="py-1 px-2 rounded-lg focus:outline-none  border border-gray-400" type="password" name="password" placeholder="Enter your password" />
-        <button className="bg-gray-400 py-2 text-white font-bold rounded">Login</button>
-    </form>
+    <div className="">
+        <h2 className="text-center text-slate-700">Login</h2>
+
+        <form className="pt-10 px-8 sm:px-32 max-w-3xl mx-auto flex flex-col gap-2">
+            <input className="py-1 px-2 rounded-lg focus:outline-none  border border-gray-400" type="email"name="email" placeholder="Enter your e-mail" />
+            <input className="py-1 px-2 rounded-lg focus:outline-none  border border-gray-400" type="password" name="password" placeholder="Enter your password" />
+            <button className="bg-gray-400 py-2 text-white font-bold rounded">Login</button>
+        </form>
+    </div>
   )
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,23 +2,18 @@ import React from 'react'
 
 export default function Register() {
   return (
-    <form action="">
-        <div className="">
-            <label htmlFor="name">Name: </label>
-            <input type="text" name='name' required />
-        </div>
-        <div className="">
-            <label htmlFor="username">Username: </label>
-            <input type="text" name='username' required />
-        </div>
-        <div className="">
-            <label htmlFor="name">Email: </label>
-            <input type="email" name='email' required />
-        </div>
-        <div className="">
-            <label htmlFor="name">Password: </label>
-            <input type="password" name='password' required />
-        </div>
-    </form>
+    <div className="">
+        <h2 className="text-center text-slate-700 m-0">Join the community</h2>
+
+        <form className="pt-10 px-8 sm:px-32 max-w-3xl mx-auto flex flex-col gap-2">
+            <input className="py-1 px-2 rounded-lg focus:outline-none  border border-gray-400" type="text"name="username" placeholder="Enter a username" />
+
+            <input className="py-1 px-2 rounded-lg focus:outline-none  border border-gray-400" type="email"name="email" placeholder="Enter your e-mail" />
+
+            <input className="py-1 px-2 rounded-lg focus:outline-none  border border-gray-400" type="password" name="password" placeholder="Enter a password" />
+
+            <button className="bg-gray-400 py-2 text-white font-bold rounded">Register</button>
+        </form>
+    </div>
   )
 }
